6f446c003bf313c90924d19ac72a4966f1e327af
richard morris <richard@corkmate.com>
2024-03-10 09:00:15 +0000

    black reformatting

--- deBruijnGraph/deBruijnGraph/__init__.py
+++ deBruijnGraph/deBruijnGraph/__init__.py
@@ -3,18 +3,20 @@ De Bruijn Graph library
 """
 __version__ = "0.1.0"
 
+
 def create(sequence: str, k: int) -> dict:
     """
     Create a De Bruijn Graph from a sequence and k-mer length
     """
     graph = {}
     for i in range(len(sequence) - k + 1):
-        kmer = sequence[i:i+k]
+        kmer = sequence[i : i + k]
         if kmer[:-1] not in graph:
             graph[kmer[:-1]] = [kmer[1:]]
         else:
             graph[kmer[:-1]].append(kmer[1:])
     return graph
+
 
 def to_mermaid(graph: dict) -> str:
     """
@@ -27,4 +29,3 @@ def to_mermaid(graph: dict) -> str:
             mermaid += f"    {node} -->|{neighbor[-1]}| {neighbor}\n"
     mermaid += "end\n"
     return mermaid
-
